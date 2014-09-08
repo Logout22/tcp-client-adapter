1428c1dc46dc77447aafef5d375be74a131c1d49
Martin Unzner <abmelden22@gmx.de>
2014-09-08 22:04:13 +0200

    Pseudo-code for data paths between endpoints

--- nwfns.c
+++ nwfns.c
@@ -12,23 +12,6 @@
 
 #include "freeatexit.h"
 
-socket_pair *alloc_socket_pair() {
-    socket_pair *result =
-        (socket_pair*) calloc(1, sizeof(socket_pair));
-    assert(result != NULL);
-
-    result->sock1 = -1;
-    result->sock2 = -1;
-    return result;
-}
-
-void free_socket_pair(void *t) {
-    socket_pair *target = (socket_pair *) t;
-    close(target->sock1);
-    close(target->sock2);
-    free(target);
-}
-
 int bind_socket(char const *address, uint16_t const port, bool use_ipv6) {
     struct addrinfo *addresses = NULL;
     struct addrinfo hints = {
@@ -74,7 +57,64 @@ int bind_socket(char const *address, uint16_t const port, bool use_ipv6) {
     return sock;
 }
 
-void connect_sockets(struct event_base *evbase, socket_pair *serversocks) {  }
+void readcb(struct bufferevent *bev, void *ctx) {
+    bufferevent_read_buffer(bev, bufferevent_get_output(sth ctx));
+}
+
+void writecb(struct bufferevent *bev, void *ctx) {
+    bufferevent_write_buffer(bev, bufferevent_get_input(sth ctx));
+}
+
+void eventcb() {
+    struct info *inf = ctx;
+    struct evbuffer *input = bufferevent_get_input(bev);
+    int finished = 0;
+
+    if (events & BEV_EVENT_EOF) {
+        size_t len = evbuffer_get_length(input);
+        printf("Got a close from %s.  We drained %lu bytes from it, "
+            "and have %lu left.\n", inf->name,
+            (unsigned long)inf->total_drained, (unsigned long)len);
+        finished = 1;
+    }
+    if (events & BEV_EVENT_ERROR) {
+        printf("Got an error from %s: %s\n",
+            inf->name, evutil_socket_error_to_string(EVUTIL_SOCKET_ERROR()));
+        finished = 1;
+    }
+    if (finished) {
+        free(ctx);
+        bufferevent_free(bev);
+    }
+}
+
+void new_client_cb(evutil_socket_t sock1, short what, void *s2) {
+    buffered_socket_pair *bla = allocate...
+    struct bufferevent **other_sock = (struct bufferevent **) s2;
+    int cltsock = accept(sock1);
+    assert(cltsock >= 0);
+    free_object_at_exit(free_socket, cltsock);
+    evutil_make_socket_nonblocking(cltsock);
+
+    struct bufferevent *clt_bufevent =
+        bufferevent_socket_new(
+                evbase,
+                cltsock,
+                BEV_OPT_CLOSE_ON_FREE |  BEV_OPT_DEFER_CALLBACKS);
+    free_object_at_exit(tcpbridge_free_bufferevent, clt_bufevent);
+    bufferevent_setcb(bev, readcb, writecb, eventcb, newstruct);
+    bufferevent_enable(bev, EV_READ|EV_WRITE);
+
+}
+
+void connect_sockets(struct event_base *evbase, socket_pair *serversocks) {
+    evutil_make_socket_nonblocking(serversock);
+    struct event *sock_event = event_new(evbase, serversock, EV_READ,
+            new_client_cb, (void*) other_server_sock);
+    free_object_at_exit(tcpbridge_free_event, sock_event);
+    event_add(sock_event, NULL);
+    serversocks->sock1
+}
 
 void tcpbridge_free_eventbase(void *base) {
     event_base_free((struct event_base*) base);
--- nwfns.h
+++ nwfns.h
@@ -7,13 +7,6 @@
 #include <event2/event.h>
 #include "tcpbridge_options.h"
 
-typedef struct socket_pair {
-    int sock1;
-    int sock2;
-} socket_pair;
-socket_pair *alloc_socket_pair();
-void free_socket_pair(void *t);
-
 struct event_base *setup_network(tcpbridge_options *opts);
 int bind_socket(char const *address, uint16_t const port, bool use_ipv6);
 
